0d1f61513b1b03111f57830210aa3daccd55b643
Emmanuel Olatunde <56281174+Emanoid@users.noreply.github.com>
2020-04-15 23:03:10 -0400

    Add files via upload
    Updated!!

--- Insersion_Sort.py
+++ Insersion_Sort.py
@@ -1,15 +1,14 @@
-def insertionSort(nlist):
-   for index in range(1,len(nlist)):
 
-     currentvalue = nlist[index]
-     position = index
-
-     while position>0 and nlist[position-1]>currentvalue:
-         nlist[position]=nlist[position-1]
-         position = position-1
-
-     nlist[position]=currentvalue
+def insertionSort(arr):
+    for j in range(1, len(arr)): 
+        key = arr[j] 
+        i = j-1
+        while i >=0 and key < arr[i] : 
+            arr[i+1] = arr[i] 
+            i -= 1
+        arr[i+1] = key  
 
 nlist = [14,46,43,27,57,41,45,21,70]
 insertionSort(nlist)
 print(nlist)
+
